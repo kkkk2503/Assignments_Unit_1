46f5079c7233543539724bf7faddee5ec0e2e514
kkkk2503 <145631826+kkkk2503@users.noreply.github.com>
2024-10-28 14:38:50 +0530

    Add files via upload

---
+++ bitwise_operator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    a=20;
+    b=10;
+    printf(" a&b %d\n",a&b);
+    printf("a|b %d\n",a|b);
+    printf(" a^b %d\n",a^b);
+    printf(" ~a %d\n",~a);
+    printf("a>>b %d\n",a>>b);
+    printf(" a<<b %d\n",a<<b);
+    return 0;
+    //program for relational operators
+}
