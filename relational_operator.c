b727bf2154f1dc865e32e2ca98cee4a87729bb74
kkkk2503 <145631826+kkkk2503@users.noreply.github.com>
2024-10-28 13:08:18 +0530

    Add files via upload

---
+++ relational_operator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    a=20;
+    b=10;
+    printf("%d\n a>b",a>b);
+    printf("%d\n a<b",a<b);
+    printf("%d\n a>=b",a>=b);
+    printf("%d\n a<=b",a<=b);
+    printf("%d\n a==b",a==b);
+    printf("%d\n a!=b",a!=b);
+    return 0;
+    //program for relational operators
+}
