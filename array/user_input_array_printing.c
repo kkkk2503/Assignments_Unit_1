6bc88af7a550e5b1d76e57c064c9adba8a841075
kkkk2503 <kshitijkhisti25@gmail.com>
2024-11-18 14:56:52 +0530

    Update user_input_array_printing.c

--- array/user_input_array_printing.c
+++ array/user_input_array_printing.c
@@ -3,10 +3,9 @@
 int main()
 {
     int i,array[5];
-    printf("enter the elements of array");
+    printf("enter the elements of array\n");
     for ( i = 0; i < 5; i++)
     {
- 
         scanf("%d",&array[i]);
     }
     for ( i = 0; i < 5; i++)
