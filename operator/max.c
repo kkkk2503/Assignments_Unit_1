805dec6d1d0daa79ad7f017645d8ac3ffe85c094
kkkk2503 <kshitijkhisti25@gmail.com>
2024-12-02 15:07:09 +0530

    Update max.c

--- operator/max.c
+++ operator/max.c
@@ -2,19 +2,22 @@
 int main()
 {
 int a,b,c;
+printf("enter your first number");
 scanf("%d",&a);
+printf("enter your second number");
 scanf("%d",&b);
+printf("enter your third number");
 scanf("%d",&c);
 if(a>b&&b>c)
 {
-    printf("a is max");
+    printf("a is max with a value of %d",a);
 }
 if(b>a&&b>c)
 {
-    printf("b is max");
+    printf("b is max with a value of %d",b);
 }
 if(c>a&&c>b)
 {
-    printf("c is max");
+    printf("c is max with a value of %d",c);
 }
 }
