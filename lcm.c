27306094ac51a05364b7563495d592df67aa9bd4
kkkk2503 <kshitijkhisti25@gmail.com>
2024-12-28 13:58:54 +0530

    Update lcm.c

---
+++ lcm.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1,num2,lcm,a;
+    printf("enter first number");
+    scanf("%d",&num1);
+    printf("enter second number");
+    scanf("%d",&num2);
+    a=(num1>num2)?num1:num2;
+    for (lcm = a; lcm <= num1 * num2; lcm += a) {
+        if (lcm % num1 == 0 && lcm % num2 == 0) {
+            break;
+        }
+    }
+    printf("LCM of %d and %d is: %d\n", num1, num2, lcm);
+    return 0;
+}
