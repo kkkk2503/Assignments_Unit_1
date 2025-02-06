9bf31ec846324c12dc6126d29938ef88b73ae1a2
kkkk2503 <kshitijkhisti25@gmail.com>
2025-02-06 15:58:33 +0530

    Update 15.c

--- lab experiments/15.c
+++ lab experiments/15.c
@@ -1,12 +1,66 @@
 #include<stdio.h>
 
-int add(int a, int b)
+int add()
 {
-printf("enter first number: ");
-scanf("%d", &a);
-printf("enter second number: ");
-scanf("%d", &b);
-printf("addition is %d ",a + b);
+int r,c,i,j,;
+    
+    printf("ENTER ROWS OF MATRIX ");
+    scanf("%d",&r);
+    printf("enter columns of matrix ");
+    scanf("%d",&c);
+    int a[r][c],b[r][c];
+    
+    for (i = 0; i < r1; i++) 
+    {
+    for (j = 0; j < c1; j++) 
+        {
+        c[i][j] = 0;
+        }
+    }
+    printf("ENTER ELEMENTS OF 1ST MATRIX \n");
+    for(i=0;i<r1;i++)
+    {
+        for(j=0;j<c1;j++)
+        {
+    printf("enter element of array [%d] [%d]",i+1,j+1);
+    scanf("%d",&a[i][j]);
+        }
+    }
+    printf("ENTER ELEMENTS OF 2nd MATRIX \n");
+    for(i=0;i<r2;i++)
+    {
+        for(j=0;j<c2;j++)
+        {
+    printf("enter element of array [%d] [%d]",i+1,j+1);
+    scanf("%d",&b[i][j]);
+        }
+    }
+    printf("this is matrix 1 \n");
+    for(i=0;i<r1;i++)
+    {
+        for(j=0;j<c1;j++)
+        {
+    printf(" %d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n this is matrix 2\n");
+    for(i=0;i<r2;i++)
+        {
+        for(j=0;j<c2;j++)
+        {
+    printf(" %d ",b[i][j]);
+        }
+        printf("\n");
+        }
+
+        for(i=0;i<r;i++)
+        {
+            for(j=0;j<c;j++)
+            {
+                c[i][j]=a[i][j]+b[i][j];
+            }
+        }
 }
 
 int matricsmultiply()
@@ -95,12 +149,12 @@ int matricsmultiply()
 int main()
 {
     int choice;
-    printf("enter number 1 for addition of two numbers or 2 for matrix multiplication: ");
+printf("enter number 1 for addition of matrices or 2 for matrix multiplication: ");
     scanf("%d", &choice);
     switch (choice)
     {
     case 1:
-    add(0,0);
+    add();
         break;
     case 2:
     matricsmultiply();
